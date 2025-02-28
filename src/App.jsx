d93c7caab9632a700d6cd5f3a98f37eb1bf54d7f
Davies Wabuluka <dwabuluka@gmail.com>
2025-02-28 10:31:19 -0600

    swe frontend project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>
+    </>
+  );
+}
+
+export default App;
